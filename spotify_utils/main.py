09fac4b33c52a080f082f888f91f21fdc3955125
Fabian Eulitz <dev@sustineo.de>
2022-03-30 16:19:08 +0200

    Added dynaconf configuration

--- spotify_utils/main.py
+++ spotify_utils/main.py
@@ -1,7 +1,17 @@
+from typing import Type
 import typer
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from config import settings
 
-def main(name: str):
-    typer.echo(f"Hello {name}")
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=settings.SPOTIPY_CLIENT_ID,
+                     client_secret=settings.SPOTIPY_CLIENT_SECRET, redirect_uri=settings.SPOTIPY_REDIRECT_URI, scope=settings.SCOPE))
+
+
+def main():
+    test = sp.current_user_playlists()
+    print(test)
+
 
 if __name__ == "__main__":
     typer.run(main)
