69c96e7a9d53dba2523cc6bfddc4f6bda5e12ca4
fabieu <dev@sustineo.de>
2022-05-02 22:39:35 +0200

    Final release of spotify-utils v1.0.0 (#2)
    * Added new commands
      - list
      - duplicates
      - export
    * Added comfort options like --quiet and --verbose
    * Added HTML template for playlist export
    * Added test cases for all commands and various options
    * Added launch option for export command (defaults to true)
    * Added dynaconf for configuration
    * Added documentation
    * Changed licence from MIT to GPL-3.0
    * Added vermin for minimal python version check
    * Fixed an issue with the spotipy cache file
    * Added dynaconf validator
    * Added new command
      - version
    * Updated dependencies

--- spotify_utils/main.py
+++ spotify_utils/main.py
@@ -1,7 +1,24 @@
+# Built-in modules
+import importlib.metadata
+
+# PyPi modules
 import typer
 
-def main(name: str):
-    typer.echo(f"Hello {name}")
+# Local modules
+from spotify_utils.src import playlists
+
+# Global variables
+__version__ = importlib.metadata.version("spotify-utils")
+
+# Initialize Typer and populate commands
+app = typer.Typer(help=f"spotify-utils v{__version__}")
+app.add_typer(playlists.app, name="playlists")
+
+
+@app.command()
+def version():
+    typer.echo(f"spotify-utils v{__version__}")
+
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
