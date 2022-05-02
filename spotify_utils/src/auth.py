dbe1dd9bac542343b32e4a9e84df09a80edee8be
Fabian Eulitz <dev@sustineo.de>
2022-05-02 13:06:25 +0200

    Added documentation
    - Changed licence from MIT to GPL-3.0
    - Added vermin for minimal python version check
    - Sticky navbar on HTML template

--- spotify_utils/src/auth.py
+++ spotify_utils/src/auth.py
@@ -7,8 +7,7 @@ from spotify_utils.config import settings
 
 # Local modules
 
-AUTH_REDIRECT_URI = "http://localhost:60011"  # Redirect URL for the Spotify Authorization Code Flow
 SCOPES = ["playlist-read-private"]  # Required scopes for the Spotify API
 
 session = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=settings.CLIENT_ID,
-                                                    client_secret=settings.CLIENT_SECRET, redirect_uri=AUTH_REDIRECT_URI, scope=",".join(SCOPES)))
+                                                    client_secret=settings.CLIENT_SECRET, redirect_uri=settings.REDIRECT_URI, scope=",".join(SCOPES)))
