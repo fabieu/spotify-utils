82a679f28944bd66c814134733b564bf2f768bc7
Fabian Eulitz <dev@sustineo.de>
2022-05-02 22:39:35 +0200

    Final release of spotify-utils v1.0.0 (#2)
    * Added new commands
      - list
      - duplicates
      - export
    * Added comfort options like --quiet and --verbose
    * Added HTML template for playlist export
    * Added test cases for all commands and various options
    * Added launch option for export command (defaults to true)
    * Added dynaconf for configuration
    * Added documentation
    * Changed licence from MIT to GPL-3.0
    * Added vermin for minimal python version check
    * Fixed an issue with the spotipy cache file
    * Added dynaconf validator
    * Added new command
      - version
    * Updated dependencies

---
+++ tests/test_playlists.py
@@ -0,0 +1,67 @@
+# Build-in modules
+import json
+from pathlib import Path
+from datetime import date
+
+# PyPi modules
+from typer.testing import CliRunner
+
+# Local modules
+from spotify_utils.main import app
+
+runner = CliRunner()
+
+
+def test_list():
+    result = runner.invoke(app, ["playlists", "list"])
+    assert result.exit_code == 0
+
+
+def test_list_json():
+    result = runner.invoke(app, ["playlists", "list", "--json"])
+    assert result.exit_code == 0
+    assert json.loads(result.stdout) != None
+
+
+def test_duplicates():
+    result = runner.invoke(app, ["playlists", "duplicates"])
+    assert result.exit_code == 0
+    assert result.stdout != None
+
+
+def test_duplicates_quiet():
+    result = runner.invoke(app, ["playlists", "duplicates", "--quiet"])
+    assert result.exit_code == 0
+    assert len(result.stdout) == 0
+
+
+def test_export_json():
+    try:
+        result = runner.invoke(app, ["playlists", "export", "--json", "--no-launch"])
+        assert result.exit_code == 0
+
+        # Check if file hase been created
+        path = Path().cwd()
+        outpath = path / f"playlist_export_{date.today()}.json"
+        assert outpath.exists()
+
+        # Test if file content is valid JSON
+        with open(outpath) as f:
+            assert json.load(f) != None
+    finally:
+        outpath.unlink()
+
+
+def test_export_html():
+    try:
+        result = runner.invoke(app, ["playlists", "export", "--html", "--no-launch"])
+        print(result)
+        assert result.exit_code == 0
+
+        # Check if file hase been created and is not empty
+        path = Path().cwd()
+        outpath = path / f"playlist_export_{date.today()}.html"
+        assert outpath.exists()
+        assert outpath.stat().st_size != 0
+    finally:
+        outpath.unlink()
